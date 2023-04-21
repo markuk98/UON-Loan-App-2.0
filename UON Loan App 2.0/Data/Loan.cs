5ad0103c359c20aeca70ed29164e5a1567d02d26
markuk98 <mark_uk98@hotmail.com>
2023-04-21 11:52:10 +0100

    .

---
+++ UON Loan App 2.0/Data/Loan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoanAppDataAccess
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
